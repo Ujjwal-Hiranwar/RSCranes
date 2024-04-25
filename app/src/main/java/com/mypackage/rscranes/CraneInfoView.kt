303336514fa15c376f563821d7dd521ad2549767
Vidhan <vidhandahatkar@gmail.com>
2024-04-25 23:27:31 +0530

    EditCraneInfoView

--- app/src/main/java/com/mypackage/rscranes/CraneInfoView.kt
+++ app/src/main/java/com/mypackage/rscranes/CraneInfoView.kt
@@ -1,5 +1,6 @@
 package com.mypackage.rscranes
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
@@ -15,7 +16,6 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.mypackage.rscranes.SplashScreenActivity.adminUser.isAdmin
-import com.mypackage.rscranes.databinding.ActivityAdminAddCraneBinding
 import com.mypackage.rscranes.databinding.ActivityCraneInfoViewBinding
 
 class CraneInfoView : AppCompatActivity() {
@@ -24,6 +24,7 @@ class CraneInfoView : AppCompatActivity() {
     private lateinit var db: FirebaseDatabase
     private lateinit var databaseReference: DatabaseReference
 
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_crane_info_view)
@@ -33,19 +34,78 @@ class CraneInfoView : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
         db = FirebaseDatabase.getInstance()
         databaseReference = db.reference.child("Crane details").child(receivedValue.toString())
-        Toast.makeText(this, isAdmin.toString(), Toast.LENGTH_SHORT).show()
         if (isAdmin) {
             binding.edit.visibility = View.VISIBLE
             binding.editImg.visibility = View.VISIBLE
+        }else{
+
+        Toast.makeText(this, isAdmin.toString(), Toast.LENGTH_SHORT).show()
         }
         binding.edit.setOnClickListener {
-          val intent = Intent(this,Edit_Crane_Info::class.java)
-            intent.putExtra("model_name_key",binding.cranemodelname.toString())
+          val intent = Intent(this,EditCraneInfoView::class.java)
+            intent.putExtra("model_name_key",receivedValue.toString())
             startActivity(intent)
         }
         binding.back.setOnClickListener {
             finish()
         }
+
+        databaseReference.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+
+                    var i = 0
+                    for (snapshot in snapshot.children) {
+                        val crane = snapshot.getValue()
+                        Log.d("todo", crane.toString())
+                        when (i) {
+                            0 -> {
+                                binding.craneboomlength.text = crane.toString()
+                                i++
+                            }
+                            1 -> {
+                                binding.cranecapacity.text = crane.toString()
+                                i++
+                            }
+                            2 -> {
+                                i++
+                            }
+                            3 -> {
+                                binding.craneflyjib.text = crane.toString()
+                                i++
+                            }
+                            4 -> {
+                                i++
+                            }
+                            5 -> {
+                                binding.cranelocation.text = crane.toString()
+                                i++
+                            }
+                            6 -> {
+                                binding.cranemodelname.text = crane.toString()
+                                i++
+                            }
+                            7 -> {
+                                binding.cranestatus.text = crane.toString()
+                                i++
+                            }
+                            else -> {
+                                binding.sellOrRentbtn.text = crane.toString()
+                                i++
+                            }
+                        }
+
+
+                    }
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(ContentValues.TAG, "Failed to read value.", error.toException())
+            }
+        })
 
 
         binding.sellOrRentbtn.setOnClickListener {
--- app/src/main/java/com/mypackage/rscranes/EditCraneInfoView.kt
+++ app/src/main/java/com/mypackage/rscranes/EditCraneInfoView.kt
@@ -9,8 +9,6 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
@@ -18,12 +16,10 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.mypackage.rscranes.databinding.ActivityCraneInfoViewBinding
-import com.mypackage.rscranes.databinding.ActivityEditCraneInfoBinding
-import java.sql.Types.NULL
+import com.mypackage.rscranes.databinding.ActivityEditCraneInfoViewBinding
 
-class Edit_Crane_Info : AppCompatActivity() {
-    private lateinit var binding :ActivityEditCraneInfoBinding
+class EditCraneInfoView : AppCompatActivity() {
+    private lateinit var binding :ActivityEditCraneInfoViewBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var db: FirebaseDatabase
     private lateinit var databaseReference: DatabaseReference
@@ -31,57 +27,68 @@ class Edit_Crane_Info : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-//        setContentView(R.layout.activity_edit_crane_info)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_crane_info_view)
         val receivedValue = intent.getStringExtra("model_name_key")
         Log.d("todo", receivedValue.toString())
         auth = FirebaseAuth.getInstance()
         db = FirebaseDatabase.getInstance()
         databaseReference = db.reference.child("Crane details").child(receivedValue.toString())
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_crane_info)
 
         databaseReference.addValueEventListener(object : ValueEventListener {
 
             override fun onDataChange(snapshot: DataSnapshot) {
                 if (snapshot.exists()) {
 
-                    var i = 0;
+                    var i = 0
                     for (snapshot in snapshot.children) {
                         val crane = snapshot.getValue()
                         Log.d("todo", crane.toString())
-                        if (i == 0) {
-                            binding.editcraneboomlength.setText(crane.toString())
-                            i++
-                        } else if (i == 1) {
-                            binding.editcranecapacity.setText(crane.toString())
-                            i++
-                        } else if (i == 2) {
-                            binding.des.setText(crane.toString())
-                            i++
-                        } else if (i == 3) {
-                            binding.editcraneflyjib.setText(crane.toString())
-                            i++
-                        } else if (i == 4) {
-//                          Image section
-                            i++
-                        } else if (i == 5) {
-                            binding.editcranelocation.setText(crane.toString())
-                            i++
-                        } else if (i == 6) {
-                            binding.editcranemodel.setText(crane.toString())
-                            i++
-                        } else if (i == 7) {
-                            binding.editstatus.setText(crane.toString())
-                            i++
-                        } else if (i == 8) {
-                            binding.editType.setText(crane.toString())
-                            i++
-                        } else {
+                        when (i) {
+                            0 -> {
+                                binding.editcraneboomlength.setText(crane.toString())
+                                i++
+                            }
+                            1 -> {
+                                binding.editcranecapacity.setText(crane.toString())
+                                i++
+                            }
+                            2 -> {
+                                binding.des.setText(crane.toString())
+                                i++
+                            }
+                            3 -> {
+                                binding.editcraneflyjib.setText(crane.toString())
+                                i++
+                            }
+                            4 -> {
+                    //                          Image section
+                                i++
+                            }
+                            5 -> {
+                                binding.editcranelocation.setText(crane.toString())
+                                i++
+                            }
+                            6 -> {
+                                binding.editcranemodel.setText(crane.toString())
+                                i++
+                            }
+                            7 -> {
+                                binding.editstatus.setText(crane.toString())
+                                i++
+                            }
+                            8 -> {
+                                binding.editType.setText(crane.toString())
+                                i++
+                            }
+                            else -> {
+                            }
                         }
 
 
                     }
                 }
+
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -90,6 +97,7 @@ class Edit_Crane_Info : AppCompatActivity() {
         })
 
         binding.addcrane.setOnClickListener {
+
             val model = binding.editcranemodel.text.trim().toString()
             val location = binding.editcranelocation.text.trim().toString()
             val capacity = binding.editcranecapacity.text.trim().toString()
@@ -99,22 +107,24 @@ class Edit_Crane_Info : AppCompatActivity() {
             val type = binding.editType.text.trim().toString()
             val description = binding.des.text.trim().toString()
 
+            uploadImageToFirebase(model,location,capacity,boomLength,flyjib,status,type,description)
+
             if (model.isNotEmpty() && location.isNotEmpty() && capacity.isNotEmpty() && boomLength.isNotEmpty() && flyjib.isNotEmpty() && status.isNotEmpty() && type.isNotEmpty()) {
 
 //                checkUser(currentUser.toString())
 
                 Toast.makeText(
-                    this@Edit_Crane_Info,
+                    this@EditCraneInfoView,
                     SplashScreenActivity.adminUser.isAdmin.toString(),
                     Toast.LENGTH_SHORT
                 )
                     .show()
 
                 if (SplashScreenActivity.adminUser.isAdmin) {
-                    startActivity(Intent(this@Edit_Crane_Info, AdminHomeActivity::class.java))
+                    startActivity(Intent(this@EditCraneInfoView, AdminHomeActivity::class.java))
                 } else {
                     Toast.makeText(
-                        this@Edit_Crane_Info,
+                        this@EditCraneInfoView,
                         "Something went wrong in AdminAddCrane",
                         Toast.LENGTH_LONG
                     ).show()
@@ -122,39 +132,42 @@ class Edit_Crane_Info : AppCompatActivity() {
             } else {
                 Toast.makeText(this, "Please fill all required details", Toast.LENGTH_SHORT).show()
             }
-            fun uploadImageToFirebase(
-                model: String,
-                location: String,
-                capacity: String,
-                boomLength: String,
-                flyjib: String,
-                status: String,
-                type: String,
-                description: String
-            ) {
 
-                // Image upload successful
-
-
-                val craneDetails = CraneDetails(
-                    model,
-                    location,
-                    capacity,
-                    boomLength,
-                    flyjib,
-                    binding.uploadImg.toString(),
-                    status,
-                    type,
-                    description
-                )
-                val dataModel = dataModel(model,  binding.uploadImg.toString(), description)
-                db.getReference("Crane details").child(model).setValue(craneDetails)
-                db.getReference("Model and Image").child(model).setValue(dataModel)
-                Toast.makeText(this, "Crane details added with image", Toast.LENGTH_SHORT)
-                    .show()
-            }
         }
 
     }
+    fun uploadImageToFirebase(
+        model: String,
+        location: String,
+        capacity: String,
+        boomLength: String,
+        flyjib: String,
+        status: String,
+        type: String,
+        description: String
+    ) {
 
+        // Image upload successful
+
+
+        val craneDetails = CraneDetails(
+            model,
+            location,
+            capacity,
+            boomLength,
+            flyjib,
+//            binding.uploadImg.toString(),
+            status,
+            type,
+            description
+        )
+        val dataModel = dataModel(model,  binding.uploadImg.toString(), description)
+        db.getReference("Crane details").child(model).setValue(craneDetails)
+        db.getReference("Model and Image").child(model).setValue(dataModel)
+        Toast.makeText(this, "Crane details added with image", Toast.LENGTH_SHORT)
+            .show()
     }
+
+}
+
+
